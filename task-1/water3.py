a86ad614dd4253fb69e035c7e67a60c703e2ec17
PatTanapat <6422781342@g.siit.tu.ac.th>
2023-09-10 19:22:49 +0700

    do task 1

--- task-1/water3.py
+++ task-1/water3.py
@@ -2,8 +2,41 @@ def initial_state():
     return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return (s[0]==4 and s[1]==4)
 
 def successors(s):
     x, y, z = s
-    return []
+    if x>0:
+        vy=5-y
+        vz=3-z
+        if y<5:
+            if x>vy:
+               yield ((x-vy,5,z),vy)
+            else:
+               yield ((0,y+x,z),x)
+        if z<3:
+             if x>vz:
+               yield ((x-vz,5,z),vz)  
+             else:
+               yield ((0,y,x+z),x)
+    if y>0:
+        vz=3-z
+        yield ((x+y,0,z),y)
+        if z<3:
+            if y>vz:
+              yield ((x,y-vz,3),vz)  
+            else:
+              yield ((x,0,y+z),x)
+    if z>0:
+       vy=5-y
+       yield((x+z,y,0),z)
+       if z<3:
+          if z<vy:
+             yield((x,5,z-vy),vy)
+          else:
+             yield((x,y+z,0),z)
+             
+       
+            
+        
+    
